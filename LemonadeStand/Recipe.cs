4fb4e4b7a2fcac821a11d4a2240db0bce08f03d0
Adam M <dev.adammilbrath@gmail.com>
2019-11-27 14:24:36 -0600

    wrapping up for the evening. will continue over thanksgiving break.

--- LemonadeStand/Recipe.cs
+++ LemonadeStand/Recipe.cs
@@ -9,19 +9,24 @@ namespace LemonadeStand_3DayStarter
     class Recipe
     {
         // member variables (HAS A)
-        int amountOfLemons;
-        int amountOfSugarCubes;
-        int amountOfIceCubes;
-        double pricePerCup;
+        public int amountOfLemons;
+        public int amountOfSugarCubes;
+        public int amountOfIceCubes;
+        public double costToMakePerCup;
 
         // constructor (SPAWNER)
-        public Recipe()
+        public Recipe(int amountOfLemons, int amountOfSugarCubes, int amountOfIceCubes)
         {
+            this.amountOfLemons = amountOfLemons;
+            this.amountOfSugarCubes = amountOfSugarCubes;
+            this.amountOfIceCubes = amountOfIceCubes;
 
         }
-    
         //member methods (CAN DO)
-
+        public void CalcCostPerCup()
+        {
+            costToMakePerCup = 
+        }
 
 
     }
